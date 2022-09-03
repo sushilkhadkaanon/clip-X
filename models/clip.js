c3fc2649082ab2ab6873c09c49b0b29c9c16dd88
Sushil Khadka <sushil.khadka.anon@gmail.com>
2022-09-04 01:35:54 +0545

    Auto delete added!

--- models/clip.js
+++ models/clip.js
@@ -6,6 +6,6 @@ const clipSchema = new Schema({
   data:   String,
   date: { type: Date, default: Date.now },
 });
-
+clipSchema.index( { "lastModifiedDate": 1 }, { expireAfterSeconds: 30 } );
 const Clip = mongoose.model('clip',clipSchema);
 module.exports = Clip;
